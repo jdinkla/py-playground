6df8cbd13230e00fa4866695ebd4cd25a6228c2b
Jörn Dinkla <jdinkla@thoughtworks.com>
2020-07-24 16:54:08 +0200

    refactor mock

--- tests/mock.py
+++ tests/mock.py
@@ -1,11 +1,13 @@
 from unittest.mock import patch
 import unittest
+import attr
 
+@attr.s
 class Point3D(object):
     def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+        x = attr.ib
+        y = attr.ib
+        z = attr.ib
 
     def m(self):
         return self.x + self.y + self.y
@@ -13,7 +15,7 @@ class Point3D(object):
 def production():
     p = Point3D(1, 2, 3)
     m = p.m()
-    print("production.m=", m)
+    # print("production.m=", m)
     return m
 
 class TestStringMethods(unittest.TestCase):
@@ -22,5 +24,6 @@ class TestStringMethods(unittest.TestCase):
         instance = mockclass.return_value
         instance.m.return_value = 1999
         result = production()
-        print("result=", result)
+        self.assertEqual(result, 1999)
+        # print("result=", result)
         instance.m.assert_called()
