c93fe0913c93eac5bc8fd5aa6e6ee1077dff6caf
Jörn Dinkla <jdinkla@thoughtworks.com>
2020-07-24 12:02:03 +0200

    refactor

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
-
-
+#!/bin/sh
 
 python3.8 -m venv dev3.8/
 source dev3.8/bin/activate
 
-pip install attrs
-pip install 
+pip install -r requirements.txt
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd tests
+../dev3.8/bin/python -m unittest mock.py
